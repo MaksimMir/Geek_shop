d5f130c68d55c44e19b842b4a28d13e61a296e03
MaxM <mak-miroshnichen@yandex.ru>
2021-03-17 18:10:14 +0300

    home page

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const gulp = require("gulp");
+const browsersync = require("browser-sync");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const imagemin = require("gulp-imagemin");
+
+
+const dist = "./dist";
+
+gulp.task("copy-html", () => {
+    return gulp.src("./src/**.html")
+                .pipe(gulp.dest(dist))
+                .pipe(browsersync.stream());
+});
+
+gulp.task("build-sass", () => {
+    return gulp.src("./src/scss/main.scss")
+                .pipe(sass().on('error', sass.logError))
+                .pipe(autoprefixer())
+                .pipe(cleanCSS())
+                .pipe(gulp.dest(dist))
+                .pipe(browsersync.stream());
+});
+
+gulp.task('imagemin', function() {
+    return gulp.src('src/img/**')
+    .pipe(imagemin())
+	.pipe(gulp.dest(dist + '/img')); 
+});
+
+gulp.task("script", () => {
+    return gulp.src('src/js/script.js')
+    .pipe(gulp.dest(dist))
+    .pipe(browsersync.stream());
+});
+
+gulp.task("watch", () => {
+    browsersync.init({
+		server: dist,
+		port: 4000,
+		notify: true
+    });
+    
+    gulp.watch("./src/index.html", gulp.parallel("copy-html"));
+    gulp.watch("./src/img/**", gulp.parallel("imagemin"));
+    gulp.watch("./src/scss/**/*.scss", gulp.parallel("build-sass"));
+    gulp.watch("./src/js/script.js", gulp.parallel("script"));
+});
+
+gulp.task("build", gulp.parallel("copy-html", "imagemin", "build-sass", "script"));
+
+gulp.task("default", gulp.parallel("build", "watch"));
---
+++ src/js/script.js
@@ -0,0 +1,46 @@
+window.addEventListener("load", () => {
+    const burger = document.querySelector('.top_right-item:first-child');
+    const menu = document.querySelector('.menu');
+    const close = document.querySelector('.menu_close');
+    const cards = document.querySelectorAll('.product_card');
+    const linkOnCart = document.querySelector('.black_link');
+    const items = document.querySelectorAll('.subscrabe_comments .item');
+    let i = 0;
+
+    const black = document.createElement('div');
+    black.classList.add('black');
+    black.innerHTML = '<p class="black_link"><i class="fa fa-shopping-cart"></i>Add to Cart</p>';
+
+    burger.addEventListener('mouseenter', () => {
+        menu.classList.add('active');
+    });
+    
+    burger.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+
+    close.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+
+    for (const card of cards) {
+        card.addEventListener('mouseenter', () => {
+            card.append(black);
+        });
+
+        card.addEventListener('mouseleave', () => {
+            card.removeChild(black);
+        });
+    };
+
+    setInterval(() => {
+        items[i].classList.remove('showed');
+        i++;
+        if(i >= items.length) i = 0;
+        
+        items[i].classList.add('showed');
+    }, 5000);
+
+    
+});
+
