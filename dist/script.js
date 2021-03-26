7d04239c9a95c5755531590140ac4667a98680e8
MaxM <mak-miroshnichen@yandex.ru>
2021-03-26 14:21:54 +0300

    update plus video

--- dist/script.js
+++ dist/script.js
@@ -1,23 +1,17 @@
 window.addEventListener("load", () => {
-    const burger = document.querySelector('.top_right-item:first-child');
+    const burger = document.querySelector('.hamburger');
     const menu = document.querySelector('.menu');
     const close = document.querySelector('.menu_close');
     const cards = document.querySelectorAll('.product_card');
     const linkOnCart = document.querySelector('.black_link');
     const items = document.querySelectorAll('.subscrabe_comments .item');
-    const slides = document.querySelectorAll('.preview_slider-item');
-    const page = document.querySelector('.preview_slider-line');
-    const prev = document.querySelector('.preview_slider-btn .prev');
-    const next = document.querySelector('.preview_slider-btn .next');
+    
     let i = 0;
+    let index = 0;
 
     const black = document.createElement('div');
     black.classList.add('black');
     black.innerHTML = '<p class="black_link"><i class="fa fa-shopping-cart"></i>Add to Cart</p>';
-
-    burger.addEventListener('mouseenter', () => {
-        menu.classList.add('active');
-    });
     
     burger.addEventListener('click', () => {
         menu.classList.toggle('active');
@@ -44,6 +38,43 @@ window.addEventListener("load", () => {
         
         items[i].classList.add('showed');
     }, 5000);
+
+
+    try {
+        const slides = document.querySelectorAll('.preview_slider-box img');
+        const line = document.querySelector('.preview_slider-line');
+        const prev = document.querySelector('.prev');
+        const next = document.querySelector('.next');
+
+        let width = window.getComputedStyle(line.parentNode).width;
+
+        for (const slide of slides) {
+            slide.style.width = width;
+        };
+
+        let pos = 0;
+        line.style.width = 100 * slides.length + '%';
+
+        
+        prev.addEventListener('click', (evt) => {
+            evt.preventDefault();
+
+            if(index < slides.length - 1) index++;                       
+            slideShow(width, pos);
+        });
+
+        next.addEventListener('click', (evt) => { 
+            evt.preventDefault();
     
+            if(index > 0) index--;              
+            slideShow(width, pos); 
+        });
+    } catch (error) {}
+    
+    function slideShow(w, p) {
+        p = w.replace(/px/g, '') * index; 
+        line.style.transition = 'all 1s'; 
+        line.style.marginLeft = -p + 'px';
+    }
 });
 
